fc861e9be513157c4e75af73ba55b53a7e914846
Mesi21 <mse212002@gmail.com>
2022-10-07 16:50:59 +0300

    :test_tube: add failing test for fizzbuzz function

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -33,5 +33,19 @@ describe Solver do
     end
   end
 
+  context '#fizzbuzz function' do
+    it 'should return fizzbuzz if the number is divisible by 3 and 5' do
+      expect(@newSolver.fizzbuzz(15)).to eq 'fizzbuzz'
+    end
+    it 'should return a fizz if the number is divisible by 3' do
+      expect(@newSolver.fizzbuzz(3)).to eq 'fizz'
+    end
+    it 'should return a buzz if the number is divisible by 5' do
+      expect(@newSolver.fizzbuzz(5)).to eq 'buzz'
+    end
+    it 'should return a number if the number is not divisible by 3 or 5' do
+      expect(@newSolver.fizzbuzz(7)).to eq 7
+    end
+  end
 end
 
