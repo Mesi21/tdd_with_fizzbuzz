87a197364cc2f11e33fb57c14923b353714d5e87
Mesi21 <mse212002@gmail.com>
2022-10-07 16:13:30 +0300

    :test_tube: add failing test

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -4,4 +4,11 @@ describe Solver do
   before :each do
     @newSolver = Solver.new
   end
+
+  context '#factorial method' do
+    it 'take argument n and return the factorial for it' do
+      result = @newSolver.factorial(0)
+      expect(result).to eq 1
+    end
+  end 
 end
